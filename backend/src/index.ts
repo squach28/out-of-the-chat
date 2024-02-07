d7256b0a767810aad0815b2fdb66de214ec4a7f9
squach28 <squach3443@gmail.com>
2024-02-07 14:05:08 -0800

    added vite frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,9 @@ import express from 'express'
 const PORT = process.env.PORT || 4000
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('hello')
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
