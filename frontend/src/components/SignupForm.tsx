10f547fe1ade23f85d7a00f1d0fe182009d41d50
squach28 <squach3443@gmail.com>
2024-02-08 14:54:56 -0800

    if user leaves field empty, make border of input field red

--- frontend/src/components/SignupForm.tsx
+++ frontend/src/components/SignupForm.tsx
@@ -36,6 +36,10 @@ const SignupForm = () => {
             data: {
                 ...registration.data,
                 [e.target.name]: e.target.value
+            },
+            errors: {
+                ...registration.errors,
+                [e.target.name]: ''
             }
         })
     }
@@ -53,7 +57,18 @@ const SignupForm = () => {
                 return e
             }
         }
+    }
 
+    const onInputBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        if(validator.isEmpty(e.target.value)) {
+            setRegistration({
+                ...registration,
+                errors: {
+                    ...registration.errors,
+                    [e.target.name]: `Field is required`
+                }
+            })
+        }
     }
 
     // use firebase auth to update a user's display name
@@ -118,15 +133,16 @@ const SignupForm = () => {
         <form className="flex flex-col p-4 gap-2" action="">
             <h1 className="text-3xl font-bold my-2">Sign Up</h1>
             <label htmlFor="firstName" className="font-bold">First Name <span className="text-red-400">*</span></label>
-            <input id="firstName" name="firstName" className={`border p-1`} type="text" onChange={onInputChange} placeholder="John" />
+            <input id="firstName" name="firstName" className={`border p-1 ${registration.errors.firstName === '' ? 'border-gray-200' : 'border-red-400'}`} type="text" onBlur={onInputBlur} onChange={onInputChange} placeholder="John" />
             <label htmlFor="lastName" className="font-bold">Last Name <span className="text-red-400">*</span></label>
-            <input id="lastName" name="lastName" className={`border p-1`} type="text" onChange={onInputChange} placeholder="Doe" />
+            <input id="lastName" name="lastName" className={`border p-1 ${registration.errors.lastName === '' ? 'border-gray-200' : 'border-red-400'}`} type="text" onBlur={onInputBlur} onChange={onInputChange} placeholder="Doe" />
             <label htmlFor="email" className="font-bold">Email <span className="text-red-400">*</span></label>
-            <input id="email" name="email" className={`border p-1`} type="email" onChange={onInputChange} placeholder="johndoe@gmail.com" />
+            <input id="email" name="email" className={`border p-1 ${registration.errors.email === '' ? 'border-gray-200' : 'border-red-400'}`} type="email" onBlur={onInputBlur} onChange={onInputChange} placeholder="johndoe@gmail.com" />
             <label htmlFor="password" className="font-bold">Password <span className="text-red-400">*</span></label>
-            <input id="password" name="password" className={`border p-1`} type="password" onChange={onInputChange} placeholder="6 characters or longer" />
+            <input id="password" name="password" className={`border p-1 ${registration.errors.password === '' ? 'border-gray-200' : 'border-red-400'}`} type="password" onBlur={onInputBlur} onChange={onInputChange} placeholder="6 characters or longer" />
             <label htmlFor="confirmPassword" className="font-bold">Confirm Password <span className="text-red-400">*</span></label>
-            <input id="confirmPassword" name="confirmPassword" className={`border p-1`} type="password" onChange={onInputChange} placeholder="Must match password" />
+            <input id="confirmPassword" name="confirmPassword" className={`border p-1 ${registration.errors.confirmPassword === '' ? 'border-gray-200' : 'border-red-400'}`} type="password" onBlur={onInputBlur} onChange={onInputChange} placeholder="Must match password" />
+            {}
             <button className="font-bold rounded-md bg-green-200 shadow-md px-1 py-2 my-2" disabled={loading} onClick={onSignupClick}>{loading ? 'Loading...' : 'Sign Up'}</button>
         </form>
     )
