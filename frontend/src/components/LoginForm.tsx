ab31292f5b18b967c3197744f9674dfc9e485177
squach28 <squach3443@gmail.com>
2024-02-10 17:28:14 -0800

    implemented google login

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import { LoginCredential } from "../types/LoginCredential"
 import validator from "validator"
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+import googleIcon from '../assets/icons/googleIcon.svg'
 
 
 const LoginForm = () => {
@@ -114,6 +116,24 @@ const LoginForm = () => {
         }
 
     }
+
+    const onGoogleLoginClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        const provider = new GoogleAuthProvider()
+        try {
+            const result = await signInWithPopup(auth, provider)
+            const credential = GoogleAuthProvider.credentialFromResult(result)
+            if(credential !== null) {
+                const user = result.user
+                console.log(user)
+                navigate('/', { replace: true })
+            }
+
+        } catch(e) {
+            console.log(e)
+        }
+    }
+
     return (
         <form className="flex flex-col p-4 gap-2" action="">
             <h1 className="text-3xl font-bold my-2">Login</h1>
@@ -129,6 +149,15 @@ const LoginForm = () => {
             <input id="password" name="password" className={`border p-1 ${loginCredential.errors.password === '' ? 'border-gray-200' : 'border-red-400'}`} type="password" onBlur={onInputBlur} onChange={onInputChange} placeholder="Password"/>
             {error ? <p className="text-red-400">{error}</p> : null}
             <button className={`font-bold rounded-md shadow-md px-1 py-2 my-2 text-button-text-light ${!validateLogin(loginCredential) ? 'bg-gray-400' : 'bg-button-light'}`} disabled={loading || !validateLogin(loginCredential)} onClick={onLoginClick}>{loading ? 'Loading...' : 'Login'}</button>
+            <div className="flex justify-between px-4">
+                <Link to="/forgotPassword" className="text-end">Forgot password?</Link>
+                <Link to="/signup" replace={true} className="">Sign Up</Link>
+            </div>
+            <p className="text-center mt-4">Or, continue with</p>
+            <button className="flex justify-center items-center gap-4 bg-blue-400 text-lg rounded-md shadow-md px-1 py-2 my-2" onClick={onGoogleLoginClick}>
+                <img src={googleIcon} alt="" />
+                <p className="text-white font-bold">Google</p>
+            </button>
         </form>
     )
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 import LoginForm from "../components/LoginForm"
 import Navbar from "../components/Navbar"
 
@@ -7,10 +6,6 @@ const Login = () => {
         <div className="flex flex-col">
             <Navbar />
             <LoginForm />
-            <div className="flex justify-between px-4">
-                <Link to="/forgotPassword" className="text-end">Forgot password?</Link>
-                <Link to="/signup" replace={true} className="">Sign Up</Link>
-            </div>
          </div>
     )
 }
