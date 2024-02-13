3963b22ac7af19688c0c004f9d524a77365a416c
squach28 <squach3443@gmail.com>
2024-02-13 09:40:00 -0800

    added pictures to verifyEmail component

--- frontend/src/components/VerifyEmail.tsx
+++ frontend/src/components/VerifyEmail.tsx
@@ -2,11 +2,14 @@ import { Link, useSearchParams } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import { applyActionCode, getAuth } from "firebase/auth"
 import { useEffect, useState } from "react"
+import newMessage from '../assets/images/newMessage.svg'
+import missedChance from '../assets/images/missedChance.svg'
 
 enum VerifyEmailResult {
     SUCCESS,
     PENDING,
-    FAILED
+    FAILED,
+    ALREADY_VERIFIED
 }
 
 const VerifyEmail = () => {
@@ -14,7 +17,7 @@ const VerifyEmail = () => {
     const [params] = useSearchParams()
     const actionCode = params.get('oobCode')
 
-    useEffect(() => {
+    useEffect(() => { //TODO: need to add a fetch to check if user is already verified so firebase error doesn't occur
         const auth = getAuth()
         if(actionCode !== null) {
             applyActionCode(auth, actionCode)
@@ -33,21 +36,39 @@ const VerifyEmail = () => {
 
     switch(result) {
         case VerifyEmailResult.PENDING:
-            return null
+            return (
+                    <div>
+                        <Navbar />
+                    </div>
+            )
         case VerifyEmailResult.SUCCESS:
             return(                    
                     <div>
                         <Navbar />
-                        <h1>Success!</h1>
-                        <p>You've verified your email address.</p>
-                        <Link to="/login">Click here to log in</Link>
+                        <div className="flex flex-col gap-2 text-center p-4 mt-4">
+                            <h1 className="text-3xl font-bold">Success!</h1>
+                            <img src={newMessage} alt="" />
+                            <p className="text-xl">You've verified your email address.</p>
+                            <Link className="underline text-lg" to="/login">Click here to log in</Link>
+                            <footer className="absolute bottom-1 right-1">
+                                <a href="https://storyset.com/communication">Communication illustrations by Storyset</a>
+                            </footer>
+                        </div>
                     </div>
                 )
         case VerifyEmailResult.FAILED:
             return (            
                 <div>
                     <Navbar />
-                    <p>Something went wrong</p>
+                    <div className="flex flex-col gap-2 text-center p-4">
+                        <h1 className="text-3xl font-bold">Something went wrong</h1>
+                        <img src={missedChance} alt="" />
+                        <p className="text-2xl">Email verification failed</p>
+                        <p className="text-lg">Please try the link in your email again.</p>
+                        <footer className="absolute bottom-1 right-1">
+                                <a href="https://storyset.com/communication">Communication illustrations by Storyset</a>
+                        </footer>
+                    </div>
                 </div>
                 )
         default:
