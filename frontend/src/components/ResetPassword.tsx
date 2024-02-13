095cb5cabebd4b21728278f4a48f7f7c04e7cb8b
squach28 <squach3443@gmail.com>
2024-02-12 18:15:14 -0800

    add errors if inputs are empty, less than 6 chars, or don't match

--- frontend/src/components/ResetPassword.tsx
+++ frontend/src/components/ResetPassword.tsx
@@ -4,12 +4,31 @@ import { useSearchParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import validator from "validator"
 
+type ResetPasswordForm = {
+    data: {
+        newPassword: string
+        confirmNewPassword: string 
+    }
+    errors: {
+        newPassword: string
+        confirmNewPassword: string 
+    }
+}
+
 const ResetPassword = () => {
     const auth = getAuth()
     const [params,] = useSearchParams()
     const [email, setEmail] = useState<string | null>(null)
-    const [newPassword, setNewPassword] = useState<string>('')
-    const [confirmNewPassword, setConfirmNewPassword] = useState<string>('')
+    const [resetPasswordForm ,setResetPasswordForm] = useState<ResetPasswordForm>({
+        data: {
+            newPassword: '',
+            confirmNewPassword: ''
+        },
+        errors: {
+            newPassword: '',
+            confirmNewPassword: ''
+        }
+    })
     const actionCode = params.get('oobCode')
 
     const MINIMUM_PASSWORD_LENGTH = 6
@@ -25,26 +44,74 @@ const ResetPassword = () => {
     }, [auth, actionCode])
 
     const validatePasswordFields = () => {
-        if(validator.isEmpty(newPassword) || validator.isEmpty(confirmNewPassword)) {
+        if(validator.isEmpty(resetPasswordForm.data.newPassword) || validator.isEmpty(resetPasswordForm.data.confirmNewPassword)) {
             return false 
         }
 
-        if(newPassword.length < MINIMUM_PASSWORD_LENGTH || confirmNewPassword.length < MINIMUM_PASSWORD_LENGTH) {
+        if(resetPasswordForm.data.newPassword.length < MINIMUM_PASSWORD_LENGTH || resetPasswordForm.data.confirmNewPassword.length < MINIMUM_PASSWORD_LENGTH) {
             return false
         }
 
-        if(newPassword !== confirmNewPassword) {
+        if(resetPasswordForm.data.newPassword !== resetPasswordForm.data.confirmNewPassword) {
             return false
         }
 
         return true
     }
 
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setResetPasswordForm({
+            ...resetPasswordForm,
+            data: {
+                ...resetPasswordForm.data,
+                [e.target.name]: e.target.value
+            },
+            errors: {
+                ...resetPasswordForm.errors,
+                [e.target.name]: ''
+            }
+        })
+    }
+
+    const onInputBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        const fieldValue = e.target.value 
+        if(validator.isEmpty(fieldValue)) {
+            setResetPasswordForm({
+                ...resetPasswordForm,
+                errors: {
+                    ...resetPasswordForm.errors,
+                    [e.target.name]: 'Field is required'
+                }
+            })
+            return 
+        }
+
+        if(fieldValue.length < MINIMUM_PASSWORD_LENGTH) {
+            setResetPasswordForm({
+                ...resetPasswordForm,
+                errors: {
+                    ...resetPasswordForm.errors,
+                    [e.target.name]: 'Must be at least 6 chars'
+                }
+            })
+            return 
+        }
+
+        if(resetPasswordForm.data.newPassword !== resetPasswordForm.data.confirmNewPassword) {
+            setResetPasswordForm({
+                ...resetPasswordForm,
+                errors: {
+                    ...resetPasswordForm.errors,
+                    confirmNewPassword: 'Passwords must match'
+                }
+            })
+        }
+    }
+
     const onPasswordResetClicked = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault()
         if(actionCode !== null) {
-            console.log(confirmNewPassword)
-            confirmPasswordReset(auth, actionCode, newPassword)
+            confirmPasswordReset(auth, actionCode, resetPasswordForm.data.newPassword)
                 .then((resp) => {
                     console.log(resp)
                 })
@@ -59,10 +126,16 @@ const ResetPassword = () => {
                 <h1 className="text-3xl">Reset Password</h1>
                 {email ? <p>Reset your password for {email}</p> : null}
                 <form className="flex flex-col gap-2">
-                    <label htmlFor="newPassword">New Password</label>
-                    <input className="border p-1" id="newPassword" type="password" onChange={(e) => setNewPassword(e.target.value)} placeholder="New Password" />
-                    <label htmlFor="confirmNewPassword">Confirm New Password</label>
-                    <input className="border p-1" id="confirmNewPassword" type="password" onChange={(e) => setConfirmNewPassword(e.target.value)} placeholder="Confirm New Password" />
+                    <div className="flex gap-2 items-center">
+                        <label htmlFor="newPassword">New Password</label>
+                        {resetPasswordForm.errors.newPassword ? <p className="text-red-500">{resetPasswordForm.errors.newPassword}</p> : null}
+                    </div>
+                    <input className="border p-1" id="newPassword" name="newPassword" type="password" onChange={onInputChange} onBlur={onInputBlur} placeholder="New Password" />
+                    <div className="flex gap-2 items-center">
+                        <label htmlFor="confirmNewPassword">Confirm Password</label>
+                        {resetPasswordForm.errors.confirmNewPassword ? <p className="text-red-500">{resetPasswordForm.errors.confirmNewPassword}</p> : null}
+                    </div>
+                    <input className="border p-1" id="confirmNewPassword" name="confirmNewPassword" type="password" onChange={onInputChange} onBlur={onInputBlur} placeholder="Confirm New Password" />
                     <button className={`font-bold rounded-md bg-button-light text-white shadow-md px-1 py-2 my-2 ${!validatePasswordFields() ? 'bg-gray-400' : 'bg-button-light'}`} disabled={!validatePasswordFields()} onClick={onPasswordResetClicked}>Reset Password</button>
                 </form>
             </div>
