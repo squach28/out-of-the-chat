e91e4bbe6729cc1d3ec26f20430ae9d24788622e
squach28 <squach3443@gmail.com>
2024-02-14 13:37:16 -0800

    add plus icon to create trip button

--- frontend/src/pages/Trips.tsx
+++ frontend/src/pages/Trips.tsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar"
 import { Trip } from "../types/Trip"
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { Link, useNavigate } from "react-router-dom"
+import plusIcon from '../assets/icons/plus-solid.svg'
 
 const Trips = () => {
     const [trips, setTrips] = useState<Trip[] | null>(null)
@@ -26,7 +27,10 @@ const Trips = () => {
             <div className="p-4">
                 <div className="flex justify-between">
                     <h1 className="text-3xl font-bold">Your Trips</h1>
-                    <Link className="w-1/3 text-md bg-green-500 font-bold text-button-text-light rounded-md text-center px-3 py-2" to="/createTrip">Create Trip</Link>
+                    <Link className="w-[40%] flex justify-between items-center text-md bg-green-500 font-bold text-button-text-light rounded-md text-center px-3 py-2" to="/createTrip">
+                        <p className="text-sm">Create Trip</p>
+                        <img className="w-5 h-5" src={plusIcon} alt="plus icon" />
+                    </Link>
                 </div>
                 { trips ? 
                     <ul>
