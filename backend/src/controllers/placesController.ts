bd1a45bdcfff8eccd74c806cf2954e8a68b0decc
squach28 <squach3443@gmail.com>
2024-02-16 14:22:43 -0800

    autocomplete only returns names of places

--- backend/src/controllers/placesController.ts
+++ backend/src/controllers/placesController.ts
@@ -3,9 +3,10 @@ import type { Request, Response } from 'express'
 export const getPlaceByText = async (req: Request, res: Response): Promise<void> => {
   const { text } = req.query
   try {
-    const places = await fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${text as string}&apiKey=${process.env.PLACES_API_KEY}`)
-    const data = await places.json()
-    res.status(200).json(data)
+    const result = await fetch(`${process.env.PLACES_API_URL}/geocode/autocomplete?text=${text as string}&apiKey=${process.env.PLACES_API_KEY}`)
+    const places = await result.json()
+    const response = places.features.map((feature: any) => feature.properties.formatted)
+    res.status(200).json(response)
   } catch (e) {
     console.log(e)
   }
