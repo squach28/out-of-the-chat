247329f68b48aac87549ce43809e7bf79f65b5f8
squach28 <squach3443@gmail.com>
2024-02-22 10:21:03 -0800

    feed component shows names of posts

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
+import { Attraction } from "../types/Attraction"
+import { Restaurant } from "../types/Restuarant"
+import { Hotel } from "../types/Hotel"
+import { useParams } from "react-router-dom"
+
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
+    const [feed, setFeed] = useState<Array<Attraction | Restaurant | Hotel> | null>(null)
+    const { id } = useParams()
+    console.log(id)
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}/feed/${id}`)
+          .then(res => res.json())
+          .then(data => setFeed(data))
+    }, [id])
+    return (
+      <div>
+        <h1 className="text-4xl font-bold">Feed</h1>
+        {feed ? feed.map(post => <p>{post.name}</p>) : null}
+      </div>
+    )
 }
 
 export default Feed
