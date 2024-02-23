c1eed3041cd7f86e1e7cd8749f3aac8484e7279b
squach28 <squach3443@gmail.com>
2024-02-22 17:28:52 -0800

    allow user to change price in edit attraction modal

--- frontend/src/components/Attractions.tsx
+++ frontend/src/components/Attractions.tsx
@@ -4,7 +4,7 @@ import { Attraction } from "../types/Attraction"
 import trashIcon from '../assets/icons/trash-solid.svg'
 import editIcon from '../assets/icons/pen-to-square-solid.svg'
 import exitIcon from '../assets/icons/xmark-solid.svg'
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 
 type AttractionCardProps = {
@@ -44,29 +44,86 @@ type EditAttractionModal = {
 
 const EditAttractionModal = (editAttractionModal: EditAttractionModal) => {
 
+    const [attraction, setAttraction] = useState<Attraction>(editAttractionModal.attraction)
+    const ref = useRef<HTMLInputElement | null>(null)
+
+    // TODO: save changes instead of overwriting
+    const onSaveChangesClicked = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        setAttraction(prev => {
+            return {
+                ...prev,
+                timestamp: new Date().toISOString()
+            }
+        })
+        fetch(`${import.meta.env.VITE_API_URL}/attractions?tripId=${editAttractionModal.attraction.id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(attraction)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+        const targetName = e.target.name
+        if(targetName === 'price') {
+            setAttraction({
+                ...attraction,
+                [e.target.name]: parseInt(e.target.value)
+            })
+        } else {
+            setAttraction({
+                ...attraction,
+                [e.target.name]: e.target.value
+            })
+        }
+
+    }
+
+    const onPriceChange = () => {
+        if(ref.current) {
+            setAttraction({
+                ...attraction,
+                price: 1
+            })
+        }
+    }
+
+    const onFreeChange = () => {
+        setAttraction({
+            ...attraction,
+            price: 0
+        })
+    }
+
+    console.log(attraction)
+
     return(
-        <form className="w-full h-3/4 md:w-1/2 md:h-1/2 flex flex-col gap-3 fixed bottom-[70%] left-[50%] translate-x-[-50%] translate-y-[50%] bg-gray-200 rounded-md shadow-lg p-4">
+        <form className="w-full md:w-1/2 flex flex-col gap-3 absolute bottom-[70%] left-[50%] translate-x-[-50%] translate-y-[50%] bg-gray-200 rounded-md shadow-lg p-4">
             <div className="flex justify-between">
                 <h1 className="text-3xl">Edit Attraction</h1>
                 <img className="w-6 h-6 hover:cursor-pointer" src={exitIcon} alt="exit icon" onClick={() => editAttractionModal.toggleEditModal(undefined)} />
             </div>
             <label htmlFor="name">Name</label>
-            <input id="name" name="name" className="p-1" type="text" value={editAttractionModal.attraction.name} placeholder="Name" />
+            <input id="name" name="name" className="p-1" type="text" onChange={onInputChange} value={editAttractionModal.attraction.name} placeholder="Name" />
             <label htmlFor="name">Name</label>
-            <textarea id="description" name="description" className="p-1 resize-none" value={editAttractionModal.attraction.description} placeholder="Description" />
+            <textarea id="description" name="description" className="p-1 resize-none" onChange={onInputChange} value={editAttractionModal.attraction.description} placeholder="Description" />
             <label htmlFor="name">URL</label>
-            <input id="url" name="url" className="p-1" type="url" value={editAttractionModal.attraction.url ?? ''} placeholder="URL" />
+            <input id="url" name="url" className="p-1" type="url" onChange={onInputChange} value={editAttractionModal.attraction.url ?? ''} placeholder="URL" />
             <label htmlFor="price">Price</label>
             <div className="flex gap-2 items-center">
-                    <input name="price-group" type="radio" checked={editAttractionModal.attraction.price > 0} />
+                    <input name="price-group" type="radio" onChange={onPriceChange} value={attraction.price} checked={attraction.price > 0} />
                     <p>$</p>
-                    <input name="price" className="border p-1" type="number" placeholder="Price" />
+                    <input ref={ref} name="price" className="border p-1" type="number" onChange={onInputChange} value={attraction.price} placeholder="Price" />
                 </div>
                 <div className="flex gap-2">
-                    <input id="free" name="price-group" type="radio" checked={editAttractionModal.attraction.price === 0} value={0}/>
+                    <input id="free" name="price-group" type="radio" checked={attraction.price === 0} onChange={onFreeChange} value={0}/>
                     <label htmlFor="free">Free</label>
                 </div>
-            <button className="font-bold rounded-md shadow-md px-2 py-3 my-2 bg-orange-400 text-button-text-light">Save Changes</button>
+            <button className="mt-auto font-bold rounded-md shadow-md px-2 py-3 my-2 bg-orange-400 text-button-text-light" onClick={onSaveChangesClicked}>Save Changes</button>
         </form>
     )
 }
