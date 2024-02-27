7c96b118d610afaf03dd89cf75672d94405cb0ff
squach28 <squach3443@gmail.com>
2024-02-27 15:23:06 -0800

    added mui components to ForgotPassword page

--- frontend/src/pages/ForgotPassword.tsx
+++ frontend/src/pages/ForgotPassword.tsx
@@ -2,17 +2,62 @@ import { Link } from "react-router-dom"
 import { useState } from "react"
 import validator from "validator"
 import verifyEmailImg from '../assets/images/verifyEmail.svg'
+import { Button, TextField } from "@mui/material"
 
-const ForgotPassword = () => {
+type ForgotPasswordFormProps = {
+    error: string
+    loading: boolean
+    onPasswordResetClicked: (e: React.MouseEvent<HTMLButtonElement>, email: string) => void
+}
+
+const ForgotPasswordForm = (forgotPasswordFormProps: ForgotPasswordFormProps) => {
 
     const [email, setEmail] = useState<string>('')
-    const [loading, setLoading] = useState<boolean>()
-    const [error, setError] = useState<string>('')
-    const [success, setSuccess] = useState<boolean>(false)
 
     const onEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value)
     }
+
+    const validateEmail = (email: string) => {
+        return validator.isEmail(email)
+    }
+
+    return(
+        <form className="flex flex-col p-4 mt-8 gap-4 md:min-w-[550px] md:max-w-lg md:p-8 md:mx-auto md:shadow-md md:rounded-lg md:border">
+        <h1 className="text-3xl font-bold">Forgot Password?</h1>
+        <h2 className="text-xl">All good, it happens to the best of us</h2>
+        <p>Enter your email and we'll send instructions to reset your password.</p>
+        <TextField 
+            id="email"
+            name="email"
+            type="email"
+            label="Email"
+            onChange={onEmailChange}
+            error={forgotPasswordFormProps.error !== ''}
+            helperText={forgotPasswordFormProps.error}
+        />
+        <Button
+            variant="contained"
+            onClick={(e) => forgotPasswordFormProps.onPasswordResetClicked(e, email)}
+            disabled={forgotPasswordFormProps.loading || !validateEmail(email)}
+            sx={{ p: 1, marginTop: 1 }}
+        >
+            Reset Password
+        </Button>
+        <Button
+            variant="outlined"
+        >
+            <Link to="/login" className="text-center" replace={true}>Back to log in</Link>
+        </Button>
+    </form>        
+    )
+}
+
+const ForgotPassword = () => {
+
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string>('')
+    const [success, setSuccess] = useState<boolean>(false)
 
     const validateEmail = (email: string) => {
         return validator.isEmail(email)
@@ -30,7 +75,7 @@ const ForgotPassword = () => {
         return data
     }
 
-    const onPasswordResetClicked = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    const onPasswordResetClicked = async (e: React.MouseEvent<HTMLButtonElement>, email: string) => {
         e.preventDefault()
         if(!validateEmail(email)) {
             setError('Email is not valid')
@@ -53,27 +98,21 @@ const ForgotPassword = () => {
     }
 
     return (
-        <div>
+        <div className="md:mt-24">
             {success ?
-                <div className="flex flex-col justify-center items-center gap-2 p-4 mt-8">
+                <div className="flex flex-col justify-center items-center gap-2 p-4 mt-8 md:mt-24">
                     <p className="text-4xl font-bold text-center">Success!</p>
                     <img width={250} height={250} src={verifyEmailImg} alt="" />
                     <p className="text-xl text-center">Check your email on instructions to reset your password.</p>
-                    <Link className="text-lg font-bold underline" to="/login" replace={true}>Back to login</Link>
+                    <Button
+                        variant="outlined"
+                        sx={{ marginTop: 2 }}
+                    >
+                        <Link className="text-lg font-bold p-1" to="/login" replace={true}>Back to login</Link>
+                    </Button>
                 </div> 
                 :
-                <form className="flex flex-col p-4 gap-2">
-                    <h1 className="text-3xl font-bold">Forgot Password?</h1>
-                    <h2 className="text-xl">All good, it happens to the best of us</h2>
-                    <p>Enter your email and we'll send instructions to reset your password.</p>
-                    <div className="flex gap-2">
-                        <label className="font-bold" htmlFor="email">Email</label>
-                        {error ? <p className="text-red-500">{error}</p> : null}
-                    </div>
-                    <input id="email" className="border p-1" name="email" type="text" placeholder="Enter your email" onChange={onEmailChange} />
-                    <button className={`font-bold rounded-md bg-button-light text-white shadow-md px-1 py-2 my-2`} onClick={onPasswordResetClicked}>{loading ? 'Loading...' : 'Reset password'}</button>
-                    <Link to="/login" className="text-center" replace={true}>Back to log in</Link>
-                </form>
+                <ForgotPasswordForm error={error} onPasswordResetClicked={onPasswordResetClicked} loading={loading} />
             }
             {success ?            
                 <footer>
